10ec9ef9e69fe79d4db4d44249ab01ebceb33293
rhiensch <info@robhiensch.nl>
2013-01-28 21:01:33 +0100

    s

--- Music/Form/ViewTrackForm.cs
+++ Music/Form/ViewTrackForm.cs
@@ -12,9 +12,17 @@ namespace Music
 {
     public partial class ViewTrackForm : Form
     {
+        private User user = User.Instance;
+
         public ViewTrackForm()
         {
             InitializeComponent();
+            string gebruikersnaam = user.GetUsername();
+            string id = null;
+
+            SQLService sql = new SQLService();
+            string datum_bezocht = DateTime.Now.ToString("dd:MM:yyyy HH:mm:ss tt");
+            sql.Insert("INSERT INTO geschiedenis (user_id, track_id, datum_bezocht) VALUES  ");
         }
     }
 }
